3cafb1d2a42afcc426fd10aad55e2b3dc7556c50
kanwar <kdhillon469@gmail.com>
2024-08-12 18:11:44 +1000

    add aos ,react icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
-import Image from "next/image";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 600,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+  });
   return (
     <main className="">
       <h1>Welcome to my web agency</h1>
